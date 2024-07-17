8c6b65d12c5f16e457226714ac4820c07a67255f
Vishnu Deshmukh <133193954+VishnuDeshmukh21@users.noreply.github.com>
2024-07-17 22:48:40 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="#f0fdf4 text-dark py-4  bottom-0 w-full">
+    <footer className="#f0fdf4 text-dark py-4  mt-auto w-full">
       <div className="container mx-auto">
         <div className="flex justify-center">
           <div className="text-center">
