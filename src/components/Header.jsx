dbd467b25557a65b051389d20bbe54f5a3124d38
Vishnu Deshmukh <133193954+VishnuDeshmukh21@users.noreply.github.com>
2024-07-24 10:21:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ function Header() {
         <div className='flex items-center justify-between h-16'>
           <div className='flex items-center'>
             <Link to='/' className='text-green-700 font-bold text-4xl'>
-              Homble
+              Foodie
             </Link>
           </div>
           <div className='flex items-center space-x-4'>
